e489eb65c6dc2c1f216ebfd8b86e8548b85ece5e
EvanieWares <evaniewares@gmail.com>
2024-08-21 05:46:44 +0000

    Executing basic javascript with Node JS

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,11 @@
+/**
+ * This function displays a message to the STDOUT.
+ *
+ * @param {String} msg - The message to be displayed
+ * @author Chisomo Psyelera <https://github.com/EvanieWares>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
